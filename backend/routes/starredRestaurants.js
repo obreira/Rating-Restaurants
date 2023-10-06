eadff8c9498486a942a1f357d2f6797fab2b85cf
João Oliveira <obreiraxxx@gmail.com>
2023-10-06 22:09:39 +0100

    First commit

---
+++ backend/routes/starredRestaurants.js
@@ -0,0 +1,167 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const router = express.Router();
+const ALL_RESTAURANTS = require("./restaurants").restaurants;
+
+/**
+ * A list of starred restaurants.
+ * In a "real" application, this data would be maintained in a database.
+ */
+let STARRED_RESTAURANTS = [
+  {
+    id: "a7272cd9-26fb-44b5-8d53-9781f55175a1",
+    restaurantId: "869c848c-7a58-4ed6-ab88-72ee2e8e677c",
+    comment: "Best pho in NYC",
+  },
+  {
+    id: "8df59b21-2152-4f9b-9200-95c19aa88226",
+    restaurantId: "e8036613-4b72-46f6-ab5e-edd2fc7c4fe4",
+    comment: "Their lunch special is the best!",
+  },
+];
+
+/**
+ * Feature 6: Getting the list of all starred restaurants.
+ */
+router.get("/", (req, res) => {
+  /**
+   * We need to join our starred data with the all restaurants data to get the names.
+   * Normally this join would happen in the database.
+   */
+  const joinedStarredRestaurants = STARRED_RESTAURANTS.map(
+    (starredRestaurant) => {
+      const restaurant = ALL_RESTAURANTS.find(
+        (restaurant) => restaurant.id === starredRestaurant.restaurantId
+      );
+
+      return {
+        id: starredRestaurant.id,
+        comment: starredRestaurant.comment,
+        name: restaurant.name,
+      };
+    }
+  );
+
+  res.json(joinedStarredRestaurants);
+});
+
+/**
+ * Feature 7: Getting a specific starred restaurant.
+ */
+
+router .get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  // Find the starred restaurant with the matching id.
+  const starredRestaurant = STARRED_RESTAURANTS.find(
+    (starredRestaurant) => starredRestaurant.id === id
+  );
+
+  // If the starred restaurant doesn't exist, let the client know.
+  if (!starredRestaurant) {
+    res.sendStatus(404);
+    return;
+  }
+
+  // Find the restaurant with the matching id.
+  const restaurant = ALL_RESTAURANTS.find(
+    (restaurant) => restaurant.id === starredRestaurant.restaurantId
+  );
+
+  // If the restaurant doesn't exist, let the client know.
+  if (!restaurant) {
+    res.sendStatus(404);
+    return;
+  }
+
+  // Join the starred restaurant data with the restaurant data.
+  const joinedStarredRestaurant = {
+    id: starredRestaurant.id,
+    comment: starredRestaurant.comment,
+    name: restaurant.name,
+  };
+
+  res.json(joinedStarredRestaurant);
+});
+
+/**
+ * Feature 8: Adding to your list of starred restaurants.
+ */
+
+router.post("/", (req, res) => {
+  const { body } = req;
+  const { restaurantId, comment } = body;
+  
+  // Generate a unique ID for the new starred restaurant.
+  const id = uuidv4();
+
+  // Create the new starred restaurant.
+  const newStarredRestaurant = {
+    id,
+    restaurantId,
+    comment,
+  };
+
+  // Add the new starred restaurant to the list of starred restaurants.
+  STARRED_RESTAURANTS.push(newStarredRestaurant);
+
+  // Send the new starred restaurant back to the client.
+  res.json(newStarredRestaurant);
+});
+/**
+ * Feature 9: Deleting from your list of starred restaurants.
+ */
+
+router .delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  // Find the starred restaurant with the matching id.
+  const starredRestaurantIndex = STARRED_RESTAURANTS.findIndex(
+    (starredRestaurant) => starredRestaurant.id === id
+  );
+
+  // If the starred restaurant doesn't exist, let the client know.
+  if (starredRestaurantIndex === -1) {
+    res.sendStatus(404);
+    return;
+  }
+  
+  // Remove the starred restaurant from the list of starred restaurants.
+  STARRED_RESTAURANTS.splice(starredRestaurantIndex, 1);
+
+  // Send a 200 OK response.
+  res.sendStatus(200);
+});
+
+/**
+ * Feature 10: Updating your comment of a starred restaurant.
+ */
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  const { comment } = body;
+
+  // Find the starred restaurant with the matching id.
+  const starredRestaurant = STARRED_RESTAURANTS.find(
+    (starredRestaurant) => starredRestaurant.id === id
+  );
+
+  // If the starred restaurant doesn't exist, let the client know.
+  if (!starredRestaurant) {
+    res.sendStatus(404);
+    return;
+  }
+
+  // Update the starred restaurant's comment.
+  starredRestaurant.comment = comment;
+
+  // Send the updated starred restaurant back to the client.
+  res.json(starredRestaurant);
+});
+
+
+
+
+
+module.exports = router;
